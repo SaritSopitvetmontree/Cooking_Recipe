193b65cc7a69ea659f658f245831cc78413875d0
Sarit Sopitvetmontree <sarit.sopitvetmontree@gmail.com>
2023-05-03 14:45:11 +0900

    create the layout

---
+++ src/pages/create-recipe.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainLayout from '../components/Layout/MainLayout';
+
+const CreateRecipePage: React.FC = () => {
+  return (
+    <MainLayout>
+      {/* Add your CreateRecipePage content here */}
+    </MainLayout>
+  );
+};
+
+export default CreateRecipePage;
---
+++ src/pages/recipe-list.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainLayout from '../components/Layout/MainLayout';
+
+const CreateRecipePage: React.FC = () => {
+  return (
+    <MainLayout>
+      {/* Add your recipe list content here */}
+    </MainLayout>
+  );
+};
+
+export default CreateRecipePage;
